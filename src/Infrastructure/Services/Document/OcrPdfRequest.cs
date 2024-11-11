6a329908d840a0d818d56fba16efc431c324d48d
Sebastian <sebfischer@gmx.net>
2024-11-11 07:32:09 +0100

    pdf request response classes

---
+++ src/Infrastructure/Services/Document/OcrPdfRequest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ArquivoMate.Infrastructure.Services.Document
+{
+    public class OcrPdfRequest
+    {
+        [NotNull]
+        public IFormFile File { get; set; } = null!;
+
+        [NotNull]
+        public string[] Languages { get; set; } = ["deu", "eng"];
+    }
+}
---
+++ src/Infrastructure/Services/Document/OcrPdfResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace ArquivoMate.Infrastructure.Services.Document
+{
+    public record OcrPdfResponse
+    {
+        public required string Message { get; init; }
+
+        public bool Success { get; init; }
+
+        public required byte[]? GeneratedPdf { get; init; }
+
+        public required string Content { get; init; }
+    }
+}
