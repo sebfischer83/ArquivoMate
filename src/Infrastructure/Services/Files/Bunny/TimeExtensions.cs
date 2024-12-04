86eb2b9caefafc5cf1d477bffa326dbcf7162ecd
Sebastian Fischer <sebfischer@gmx.net>
2024-12-04 19:37:13 +0100

    bunny

---
+++ src/Infrastructure/Services/Files/Bunny/TimeExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace ArquivoMate.Infrastructure.Services.Files.Bunny
+{
+    internal static class TimeExtensions
+    {
+        internal static string ToUnixTimestamp(this DateTimeOffset time)
+            => time.ToUnixTimeSeconds().ToString();
+    }
+}
